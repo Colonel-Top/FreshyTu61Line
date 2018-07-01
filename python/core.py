fcc4d7745ce190ae79b6ce00359aa4131e37b352
Promsurin Phutthammawong <promsurinm@hotmail.com>
2018-07-01 23:25:20 +0700

    Insult me eh ?

--- python/core.py
+++ python/core.py
@@ -123,7 +123,8 @@ if  "!ban" in message and sys.argv[2] == "Ufb00beda08083bcf402fbd2160b75574":
         db.commit()
         db.close()
         line_bot_api = LineBotApi('AgIQnH2clTRGpu74YMKmHiVMvWsLo0Eg7qOum7xcoaKSjcAp24BfinEtfMTPefvMq9zYr/MnW+MLtPr8+Kd5vKL+VQIBIHWB9grdWkqr3c1vemv4bBAP5n9nRYfG988Z+s8Ps6pfh6mvo+TKMtcqIgdB04t89/1O/w1cDnyilFU=')
-        line_bot_api.push_message(message, TextSendMessage("คุณถูกแบนโดย Master Administrator\nติดต่อการปลดแบน: 0625461939 หรือพิมพ์ !sos,!aboutbot"))
+        stringsend = "คุณถูกแบนโดย Master Administrator\nติดต่อการปลดแบน: 0625461939 หรือพิมพ์ !sos,!aboutbot"
+        line_bot_api.push_message(message, TextSendMessage(stringsend))
         print("Banned ID: \n"+message+"\nSuccessfully")
         exit()
     except Exception as E:
